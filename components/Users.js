96f7fe0061eebd2a0410a71c48c764d9d836aaaa
Deven Allen <72590526+devenallen@users.noreply.github.com>
2023-04-11 14:54:42 -0400

    Add files via upload

--- components/Users.js
+++ components/Users.js
@@ -1,1 +1,20 @@
-#required
+import UserItem from './UserItem';
+import Card from './Card';
+import './Users.css';
+
+function Users(props) {
+    return (
+        <Card className="users">
+            {props.user_list.map(user => (
+            <UserItem
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                image={user.image}
+        />
+      ))}
+    </Card>
+    );
+  }
+
+export default Users;
