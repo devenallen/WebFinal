2586f0b343ef6ce57925c9f3d69ed39a740922ea
Deven Allen <72590526+devenallen@users.noreply.github.com>
2023-04-12 00:47:15 -0400

    Add files via upload

--- imleagueapp/src/App.js
+++ imleagueapp/src/App.js
@@ -1,67 +1,19 @@
-// Happy
 import './App.css';
-import { useState } from 'react';
-import Users from './components/Users';
+// import { useState } from 'react';
+// import { Link } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import LoginSignupPage from "./components/LoginSignupPage";
+import CreateItem from "./components/CreateItem";
+import Homepage from "./components/Homepage";
 
 function App() {
-  const [users, setUsers] = useState([
-    {
-      id: 'team1',
-      name: 'Hawks',
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/24/Atlanta_Hawks_logo.svg/640px-Atlanta_Hawks_logo.svg.png',
-    },
-    {
-      id: 'team2',
-      name: 'Bucks',
-      image: 'https://dq7axd795mydj.cloudfront.net/bots/bucks/assets/avatar-bg-transparent.png',
-    },
-    {
-      id: 'team3',
-      name: 'Lakers',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Los_Angeles_Lakers_logo.svg/1200px-Los_Angeles_Lakers_logo.svg.png',
-    },
-    {
-      id: 'team4',
-      name: 'Heat',
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Miami_Heat_logo.svg/640px-Miami_Heat_logo.svg.png',
-    },
-    {
-      id: 'team5',
-      name: 'Warriors',
-      image: 'https://images.ctfassets.net/a4rx79jcl3n1/139uoz1HBz6PsWh8pEqOCK/eced155325ccb92acf76962ca5d688e5/gsw-logo-1920.png',
-    },
-    {
-      id: 'team6',
-      name: 'Celtics',
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Boston_Celtics.svg/800px-Boston_Celtics.svg.png',
-    },
-  ]);
-
-  function handleDeleteUser(userId) {
-    setUsers(users.filter(user => user.id !== userId));
-  }
-
-  function handleEditUser(userId, newName) {
-    const updatedUsers = users.map(user => {
-      if (user.id === userId) {
-        return { ...user, name: newName };
-      } else {
-        return user;
-      }
-    });
-    setUsers(updatedUsers);
-  }
-
   return (
-    <div>
-      <div className="header">
-        <div className="header-center">
-          <h1 className="header-title">Intramural League Builder</h1>
-        </div>
-        <button className="header-button">Login</button>
-      </div>
-
-      <Users users={users} onDeleteUser={handleDeleteUser} onEditUser={handleEditUser} />
+    <div className="App">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login-signup" element={<LoginSignupPage />} />
+          <Route path="/create-item" element={<CreateItem />} />
+        </Routes>
     </div>
   );
 }
